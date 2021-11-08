0e3833b86265ca5c12197aa5cb8dac3abe4b20a2
Nyeedz <chr.alexfb@gmail.com>
2021-11-08 03:35:43 -0300

    integracao finalizada

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,8 @@
-import { IonFab, IonFabButton, IonFabList, IonIcon, IonToggle } from '@ionic/react';
+import { IonIcon, IonToggle } from '@ionic/react';
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { share, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
+import { endpoint, IDevice } from '../../pages/Home';
+import axios from 'axios';
 
 interface CardTheme {
   backgroundColor?: string;
@@ -9,14 +10,18 @@ interface CardTheme {
   active?: boolean;
 }
 
-export interface ICardProps {
+export interface ICardProps extends IDevice {
   icon: string;
   title: string;
   theme: CardTheme;
 }
 
-export const Card: React.FC<ICardProps> = ({ icon, title, theme }) => {
+export const Card: React.FC<ICardProps> = ({ icon, title, theme, address, id, model, place }) => {
   const [active, setActive] = useState(false);
+
+  const togglePower = (on: boolean) => {
+    axios.get(`${endpoint}/status?ip=${address}&on=${on}`).then(() => {})
+  };
 
   return (
     <StyledCard active={active} backgroundColor={theme.backgroundColor} textColor={theme.textColor}>
@@ -25,38 +30,14 @@ export const Card: React.FC<ICardProps> = ({ icon, title, theme }) => {
         <h1>{title}</h1>
         <span>{active ? 'Ligado' : 'Desligado'}</span>
       </CardInformations>
-      <IonToggle mode='ios' onClick={() => setActive(!active)}></IonToggle>
+      <IonToggle
+        mode='ios'
+        onClick={() => {
+          setActive(!active);
+          togglePower(!active);
+        }}
+      ></IonToggle>
     </StyledCard>
-  );
-};
-
-export const FabExamples: React.FC = () => {
-  return (
-    <IonFab vertical='center' horizontal='center' slot='fixed'>
-      <IonFabButton>
-        <IonIcon icon={share} />
-      </IonFabButton>
-      <IonFabList side='top'>
-        <IonFabButton>
-          <IonIcon icon={logoVimeo} />
-        </IonFabButton>
-      </IonFabList>
-      <IonFabList side='bottom'>
-        <IonFabButton>
-          <IonIcon icon={logoFacebook} />
-        </IonFabButton>
-      </IonFabList>
-      <IonFabList side='start'>
-        <IonFabButton>
-          <IonIcon icon={logoInstagram} />
-        </IonFabButton>
-      </IonFabList>
-      <IonFabList side='end'>
-        <IonFabButton>
-          <IonIcon icon={logoTwitter} />
-        </IonFabButton>
-      </IonFabList>
-    </IonFab>
   );
 };
 
@@ -74,7 +55,7 @@ export const StyledCard = styled.div<CardTheme>`
   overflow: hidden;
 
   ion-icon {
-    color: ${props => (props.active ? props.textColor : 'rgba(255, 255, 255, .2)')};
+    color: ${(props) => (props.active ? props.textColor : 'rgba(255, 255, 255, .2)')};
     z-index: 2;
   }
 
@@ -92,17 +73,17 @@ export const StyledCard = styled.div<CardTheme>`
     position: absolute;
     width: 100%;
     height: 100%;
-    background: ${props => props.backgroundColor};
+    background: ${(props) => props.backgroundColor};
     top: 0;
     left: 0;
-    transform: ${props => (props.active ? 'translateX(0%);' : 'translateX(-100%)')};
+    transform: ${(props) => (props.active ? 'translateX(0%);' : 'translateX(-100%)')};
     transition: transform 0.2s ease-in-out;
     z-index: 1;
   }
 `;
 
 const CardInformations = styled.div<CardTheme>`
-  color: ${props => (props.active ? props.textColor : 'rgba(255, 255, 255, .2)')};
+  color: ${(props) => (props.active ? props.textColor : 'rgba(255, 255, 255, .2)')};
   flex: 1;
   z-index: 2;
 
@@ -110,6 +91,7 @@ const CardInformations = styled.div<CardTheme>`
     margin: 0;
     font-size: 16px;
     font-weight: bold;
+    text-transform: capitalize;
   }
 
   span {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,51 @@
 import { IonContent, IonPage } from '@ionic/react';
 import styled from 'styled-components';
-import { Card, FabExamples, StyledCard } from '../components/Card/Card';
+import { Card, StyledCard } from '../components/Card/Card';
 import { tvOutline, beerOutline } from 'ionicons/icons';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+export const endpoint = 'http://localhost:3000';
+export interface IDevice {
+  address: string;
+  id: string;
+  model: string;
+  place: string;
+}
 
 const Home: React.FC = () => {
+  const [devices, setDevices] = useState([]);
+
+  useEffect(() => {
+    // axios.get('http://localhost:3000/status?ip=192.168.18.21').then((res) => {
+    axios.get(`${endpoint}/discover`).then((res) => {
+      setDevices(res.data.devices);
+    });
+  }, []);
+
   return (
     <IonPage id='home-page'>
       <IonContent fullscreen>
         <StyledContent>
           <PageTitle>Meu apartamento</PageTitle>
-          <PageArea>Ambientes</PageArea>
-          <Card
-            icon={tvOutline}
-            title='Sala'
-            theme={{
-              textColor: '#000',
-              backgroundColor: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgb(162 160 160) 100%)'
-            }}
-          />
-          <Card
-            icon={beerOutline}
-            title='Bar'
-            theme={{
-              textColor: '#fff',
-              backgroundColor: 'linear-gradient(90deg, rgba(250, 219, 121, 1) 0%, rgba(224, 172, 8, 1) 100%)'
-            }}
-          />
+          <PageArea>Ambiente{devices.length > 1 ? 's' : ''}</PageArea>
+          {devices.map((device: IDevice) => {
+            return (
+              <Card
+                key={device.address}
+                icon={device.place === 'sala' ? tvOutline : beerOutline}
+                title={device.place}
+                theme={{
+                  textColor: device.place === 'sala' ? '#000' : '#fff',
+                  backgroundColor:
+                    device.place === 'sala'
+                      ? 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgb(162 160 160) 100%)'
+                      : 'linear-gradient(90deg, rgba(250, 219, 121, 1) 0%, rgba(224, 172, 8, 1) 100%)',
+                }}
+                {...device}
+              />
+            );
+          })}
           <PageConfig>Configurações</PageConfig>
-          <FabExamples></FabExamples>
         </StyledContent>
       </IonContent>
     </IonPage>
@@ -52,7 +70,7 @@ const PageConfig = styled.p`
 
 const StyledContent = styled.div`
   padding: 16px;
-  
+
   ${StyledCard} + ${StyledCard} {
     margin-top: 16px;
   }
